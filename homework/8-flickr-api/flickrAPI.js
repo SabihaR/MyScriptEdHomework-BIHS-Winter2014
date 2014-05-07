799ef02514da42957001f9c4702f471c3b6b6018
Sabiha <sabihasultana@mybihs.org>
2014-05-07 00:14:27 -0400

    new homework

--- homework/8-flickr-api/flickrAPI.js
+++ homework/8-flickr-api/flickrAPI.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     
     $('#submit').click(function(){
-        var text = $('#textbox').val();
-        $('#body').prepend(text);
+    $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1c9f777eb7446f34a7261dc1a54be4b2&tags=clownfish%20sea&has_geo=1&extras=geo&format=json&jsoncallback=?", test);
     });
+  function test(data)
+  {
+    $.each(data.photos.photo, function(i,item)
+{
+             alert(item.id);
+           });
+}
+        //var text = $('#textBox').val();
+        //$('#body').prepend(text);
     
     });
