e3f2915d6ce0221695709845ef9f3409a0a734c4
Sabiha <sabihasultana@mybihs.org>
2014-05-09 16:36:42 -0400

    new homework

--- homework/8-flickr-api/flickrAPI.js
+++ homework/8-flickr-api/flickrAPI.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
-    
     $('#submit').click(function(){
-    $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1c9f777eb7446f34a7261dc1a54be4b2&tags=clownfish%20sea&has_geo=1&extras=geo&format=json&jsoncallback=?", test);
+    $("#body").html("");
+    var text = $('#textBox').val();
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1c9f777eb7446f34a7261dc1a54be4b2&tags=" + text + "&has_geo=1&extras=geo&format=json&jsoncallback=?", test);
     });
   function test(data)
   {
     $.each(data.photos.photo, function(i,item)
 {
-             alert(item.id);
+            var storeID = item.id;
+            var storeFarm = item.farm;
+            var storeSecret = item.secret;
+            var storeServer = item.server;
+            var links = "http://farm" + storeFarm + ".staticflickr.com/" + storeServer +"/"+ storeID +"_" + storeSecret + ".jpg";
+            $('#body').prepend("<img src=" + links + " height='200' width='200'>");
            });
 }
-        //var text = $('#textBox').val();
-        //$('#body').prepend(text);
     
     });
